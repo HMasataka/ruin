1cf3876c34b2d552c9d53c3b753fd602c060c740
masataka.hisasue <masataka.hisasue@gmail.com>
2025-03-05 13:07:01 +0900

    Feat mutex lock

--- ruin.go
+++ ruin.go
@@ -1,22 +1,34 @@
 package ruin
 
+import "sync"
+
 func New[T any](data []T) *Ruin[T] {
 	return &Ruin[T]{data: data}
 }
 
 type Ruin[T any] struct {
 	data []T
+	m    sync.Mutex
 }
 
 func (r *Ruin[T]) Data() []T {
+	r.m.Lock()
+	defer r.m.Unlock()
+
 	return r.data
 }
 
 func (r *Ruin[_]) Len() int {
+	r.m.Lock()
+	defer r.m.Unlock()
+
 	return len(r.data)
 }
 
 func (r *Ruin[T]) Pop() (T, error) {
+	r.m.Lock()
+	defer r.m.Unlock()
+
 	var result T
 
 	if len(r.data) == 0 {
@@ -30,10 +42,16 @@ func (r *Ruin[T]) Pop() (T, error) {
 }
 
 func (r *Ruin[T]) Insert(data T) {
+	r.m.Lock()
+	defer r.m.Unlock()
+
 	r.data = append(r.data, data)
 }
 
 func (r *Ruin[T]) Peek() (T, error) {
+	r.m.Lock()
+	defer r.m.Unlock()
+
 	var result T
 
 	if len(r.data) == 0 {
@@ -44,5 +62,8 @@ func (r *Ruin[T]) Peek() (T, error) {
 }
 
 func (r *Ruin[T]) IsEmpty() bool {
+	r.m.Lock()
+	defer r.m.Unlock()
+
 	return r.Len() == 0
 }
