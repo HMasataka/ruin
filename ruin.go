d7ec002826bc86aaf88df373d1b0c07c5891cdb7
Masataka Hisasue <masataka.hisasue@gmail.com>
2025-04-09 20:12:31 +0900

    interface経由でのアクセス

--- ruin.go
+++ ruin.go
@@ -2,7 +2,7 @@ package ruin
 
 import "sync"
 
-type Ruinable[T any] interface {
+type Ruin[T any] interface {
 	// 基本操作
 	Pop() (T, error)
 	Push(T)
@@ -12,28 +12,28 @@ type Ruinable[T any] interface {
 
 	// データ操作
 	Clear()
-	Clone() *Ruin[T]
+	Clone() Ruin[T]
 	Data() []T
 }
 
-func New[T any](data []T) *Ruin[T] {
-	return &Ruin[T]{data: data}
+func New[T any](data []T) Ruin[T] {
+	return &ruin[T]{data: data}
 }
 
-type Ruin[T any] struct {
+type ruin[T any] struct {
 	data []T
 	m    sync.Mutex
 }
 
-func (r *Ruin[T]) Data() []T {
+func (r *ruin[T]) Data() []T {
 	return r.data
 }
 
-func (r *Ruin[_]) Len() int {
+func (r *ruin[_]) Len() int {
 	return len(r.data)
 }
 
-func (r *Ruin[T]) Pop() (T, error) {
+func (r *ruin[T]) Pop() (T, error) {
 	r.m.Lock()
 	defer r.m.Unlock()
 
@@ -49,14 +49,14 @@ func (r *Ruin[T]) Pop() (T, error) {
 	return result, nil
 }
 
-func (r *Ruin[T]) Push(data T) {
+func (r *ruin[T]) Push(data T) {
 	r.m.Lock()
 	defer r.m.Unlock()
 
 	r.data = append(r.data, data)
 }
 
-func (r *Ruin[T]) Peek() (T, error) {
+func (r *ruin[T]) Peek() (T, error) {
 	var result T
 
 	if len(r.data) == 0 {
@@ -68,19 +68,19 @@ func (r *Ruin[T]) Peek() (T, error) {
 	return result, nil
 }
 
-func (r *Ruin[T]) IsEmpty() bool {
+func (r *ruin[T]) IsEmpty() bool {
 	return r.Len() == 0
 }
 
 // Clear はデータをクリアします
-func (r *Ruin[T]) Clear() {
+func (r *ruin[T]) Clear() {
 	r.m.Lock()
 	defer r.m.Unlock()
 	r.data = r.data[:0]
 }
 
 // Clone は現在のインスタンスのディープコピーを返します
-func (r *Ruin[T]) Clone() *Ruin[T] {
+func (r *ruin[T]) Clone() Ruin[T] {
 	r.m.Lock()
 	defer r.m.Unlock()
 	newData := make([]T, len(r.data))
